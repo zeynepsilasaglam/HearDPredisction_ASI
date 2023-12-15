151150707e4d388dcdc38c12fbfd63db31d92617
Anastasiya Sukach <51174032+SukachAnastasiya@users.noreply.github.com>
2023-12-15 10:58:29 +0100

    Delete kedro-environment/.venv directory

--- kedro-environment/.venv/Lib/site-packages/kedro/pipeline/__init__.py
+++
@@ -1,9 +0,0 @@
-"""``kedro.pipeline`` provides functionality to define and execute
-data-driven pipelines.
-"""
-
-from .modular_pipeline import pipeline
-from .node import node
-from .pipeline import Pipeline
-
-__all__ = ["pipeline", "node", "Pipeline"]
--- kedro-environment/.venv/Lib/site-packages/kedro/templates/pipeline/{{ cookiecutter.pipeline_name }}/nodes.py
+++
@@ -1,4 +0,0 @@
-"""
-This is a boilerplate pipeline '{{ cookiecutter.pipeline_name }}'
-generated using Kedro {{ cookiecutter.kedro_version }}
-"""
