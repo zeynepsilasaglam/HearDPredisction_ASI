0bac9381a1e40b0260ee50b6d6ee795c1386f2c8
Daria Dronova <hackedbyme410@gmail.com>
2023-12-16 21:04:15 +0100

    small changes

--- kedro-heart-disease/src/main.py
+++ kedro-heart-disease/src/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Query, Body, Depends
 from typing_extensions import Annotated# typing_ext for python <= 3.9 
-from typing import List, Any, Iterable
+from typing import List, Any, Tuple
 from pydantic import BaseModel
 from enum import Enum
 import pandas as pd
 from kedro.framework.context import KedroContext
 from kedro.framework.session import KedroSession
 from kedro.framework.startup import bootstrap_project
-
+from kedro_heart_disease.nodes import models, predict
 
 app = FastAPI()
 
@@ -23,21 +23,27 @@ class Models(str, Enum):
 def greet() -> List[str]: 
     return [model.value for model in Models]
 
-class InputEntry(BaseModel):
-    column_name: str
-    value: Any
+# class InputEntry(BaseModel):
+#     column_name: str
+#     value: Any
 
 
 #takes model name as an input and row. Request body sample: [62, 0, 0, 124, 209, 0, 1, 163, 0, 0, 2, 0, 2]
 @app.post("/predict")
 def predict(
     model_name: Annotated[Models, Query()],
-    row: Annotated[List[InputEntry], Body()]) -> Any:
+    input: Annotated[List[int], Body()]) -> Any:
     
-    pd.DataFrame(row)
-    return
+    result = models(pd.DataFrame(input))
+    print(result)
+
+    return 0
 
 
-# @app.post("/train")
-# def train()
+@app.post("/train")
+def train(model_name: Annotated[Models, Query()],
+    input: Annotated[List[int], Body()],
+    expected_output: [List[Any], Body()]) -> Any:
+
+    return 0
 
